5adc6cb4008bcd64645beb89381cfd0e0bc3447a
MucahitBilgin35 <156958560+MucahitBilgin35@users.noreply.github.com>
2026-08-22 15:47:21 +0100

    Update setup_weekend.sh

--- setup_weekend.sh
+++ setup_weekend.sh
@@ -96,6 +96,7 @@ declare -A NODES=(
     ["ComfyUI_IPAdapter_plus"]="https://github.com/cubiq/ComfyUI_IPAdapter_plus.git"
     ["x-flux-comfyui"]="https://github.com/XLabs-AI/x-flux-comfyui.git"
     ["was-node-suite-comfyui"]="https://github.com/WASasquatch/was-node-suite-comfyui.git"
+    ["ComfyUI-Pixaroma"]="https://github.com/pixaroma/ComfyUI-Pixaroma.git"
 )
 
 for name in "${!NODES[@]}"; do
